56ee1ea9c6362b5084489770227367ad75e6310d
Calvin Roberts <roberts.calvin85@gmail.com>
2023-08-02 23:00:04 +0100

    Create function to grab list items to the left side

--- script.js
+++ script.js
@@ -5,12 +5,21 @@ let lists = document.getElementsByClassName('list'),
 for (list of lists) {
   list.addEventListener('dragstart', function (e) {
     let selected = e.target;
-
+    // Select items to drag into right box
     rightBox.addEventListener('dragover', function (e) {
       e.preventDefault();
     });
     rightBox.addEventListener('drop', function (e) {
       rightBox.appendChild(selected);
+      selected = null;
+    });
+
+    // Select items to drag into left box
+    leftBox.addEventListener('dragover', function (e) {
+      e.preventDefault();
+    });
+    leftBox.addEventListener('drop', function (e) {
+      leftBox.appendChild(selected);
       selected = null;
     });
   });
